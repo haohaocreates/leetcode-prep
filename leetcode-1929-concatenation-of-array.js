91dda4e3bd3959820fd3b65462eefce9f0f95c72
haohaocreates <yechenghao.du@gmail.com>
2024-03-11 20:17:42 -0400

    POA testing

--- leetcode-1929-concatenation-of-array.js
+++ leetcode-1929-concatenation-of-array.js
@@ -2,7 +2,7 @@
 // one way is to loop through the original array that is passed in, and then re-add all of the original elements to the original array (which gives you a N runtime with N space complexity)
 // another is to use the concat function and to simply return the original array added onto itself 
 
-// higher takeaway: single pointer that loops through the array 
+// higher takeaway: single pointer that loops through the array  
 
 const getConcatenation = function(nums) {
     return [...nums, ...nums];
